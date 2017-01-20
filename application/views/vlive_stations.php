3c5bbfb70c60006b720b1c6b5a3666dd8c929686
gdegauquier <georges-xavier.degauquier@ineat-conseil.fr>
2017-01-20 07:28:19 +0100

    view station cli/serv without asynchrone

--- application/views/vlive_stations.php
+++ application/views/vlive_stations.php
@@ -1,6 +1,10 @@
+<?php
+  defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
 <html>
 <head>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/foundation/6.2.4-rc2/foundation.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4-rc2/foundation.min.css">
@@ -9,71 +13,56 @@
 
 <body>
 
+
+
+
+<div class="row"  style="background-color: #da353d;padding:1em;" >
+  <div class="small-6 columns" style="color:#FFFFFF;"><b>V-LIVE</b></div>
+  <div class="small-6 columns" style="text-align:right;"><a href="<?= site_url().'/api/v1/refresh' ?>"><img src="<?=base_url().'assets/reload.png'?>" width="32" alt="pay"/> </a></div>
+</div>
+
 <table>
   <thead>
     <tr>
-      <th width="200">id</th>
-      <th width="200">id</th>
-      <th width="200">id</th>
-      <th width="200">id</th>
-      <th width="200">id</th>
-      <th width="200">id</th>
+      
+      <th width="200"></th>
+      <th width="50"><img src="<?=base_url().'assets/bicycle.png'?>" width="32" alt="bike"/></th>
+      <th width="50"><img src="<?=base_url().'assets/parking_sign.png'?>" width="32" alt="attachs"/></th>
+      <th width="50"><img src="<?=base_url().'assets/credit_card.png'?>" width="32" alt="pay"/></th>
+      
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>Content Goes Here</td>
-      <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-    </tr>
-    <tr>
-      <td>Content Goes Here</td>
-      <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-    </tr>
-    <tr>
-      <td>Content Goes Here</td>
-      <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-      <td>Content Goes Here</td>
-    </tr>
-  </tbody>
-</table>
-
+    
 
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( $stations != null || $stations->num_rows() > 0  ){
+    
+
+  foreach (  $stations->result() as $row  ){
 
 
+      $img = "";
+      if ( $row->pay != null && $row->pay == 1 ){
+          $img = '<img src="'.base_url().'assets/credit_card.png'.'" width="32" alt="pay"/>';
+      }
 
+      echo '<tr>';
+        
+        echo '<td> <h6 style="font-size:.8rem;"><span class="badge"> '.$row->id.' </span> '.$row->name.'</h6><h6 class="subheader" style="font-size:.6rem;">'.$row->town_name.'</h6></td>';
+        echo '<td><b>'.$row->bikes.'</b></td>';
+        echo '<td><b>'.$row->attachs.'</b></td>';
+        echo '<td>'.$img.'</td>';
+      echo '</tr>';
 
-if ( $stations == null || $stations->num_rows() == 0  ){
-    return ;
+  }
+
 }
-
-
-foreach (  $stations->result() as $row     ){
-
-    echo '<br>id '.$row->id.'<br>';
-    echo 'name '.$row->name.'<br>';
-    echo 'address '.$row->address.'<br>';
-    echo 'town '.$row->town_name.'<br>';
-    echo 'bikes '.$row->bikes.'<br>';
-    echo 'attachs '.$row->attachs.'<br>';
-}
-
-
 
 ?>
-
+</tbody>
+</table>
 
 
 </body>
